da13d7f6fac965dddc6185fcf68227614fae4b62
grARM <gricode@126.com>
2016-01-17 18:52:24 +0800

    create pro

---
+++ src/crxTool.js
@@ -0,0 +1,36 @@
+/*
+ * crxTool
+ */
+;(function (factory){
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define(factory);
+	} else {
+		 
+		// Browser globals
+		crxTool = factory();
+	}
+})(function(){
+	/*
+		base
+	*/
+
+
+	/*
+		通信
+	*/
+
+	/*
+		读取页面的script内容
+	*/
+
+
+	var crxTool = {
+
+
+	};
+	return crxTools
+
+
+});
