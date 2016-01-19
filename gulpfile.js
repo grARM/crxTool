1d29eba6cbd4d33d2d2b70511c82e226c19a10d2
ruiguo208494 <ruiguo208494@sohu-inc.com>
2016-01-19 17:19:58 +0800

    添加通信功能，和获取页面变量的功能

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify');
 
+var usePath = '../../codingWS/demo/crx/FindAD/FindAD/js';
+
 //语法检查
 gulp.task('jshint', function(){
     return gulp.src('src/*.js')
@@ -14,14 +16,19 @@ gulp.task('jshint', function(){
 //压缩，合并 js
 gulp.task('minifyjs', function() {
     return gulp.src('src/*.js')      //需要操作的文件
-        .pipe(concat('crxTools.js'))    //合并所有js到main.js
+        .pipe(concat('crxTool.js'))    //合并所有js到main.js
         //.pipe(gulp.dest('dist'))       //输出到文件夹
         .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
         .pipe(uglify())    //压缩
         .pipe(gulp.dest('dist'));  //输出
 });
 
+//copy to use 
+gulp.task('moveToUse', function(){
+    return gulp.src('dist/crxTool.min.js').pipe(gulp.dest(usePath));
+});
+
 //默认命令，在cmd中输入gulp后，执行的就是这个任务(压缩js需要在检查js之后操作)
 gulp.task('default', ['jshint'], function() {
-    gulp.start('minifyjs'); 
+    gulp.start('minifyjs', 'moveToUse'); 
 });
--- src/crxTool.js
+++ src/crxTool.js
@@ -13,24 +13,118 @@
 	}
 })(function(){
 	/*
-		base
-	*/
+	 * environment check
+	 */
+	if(!chrome){
+		return;
+	}
 
+	/*
+	 *	base
+	 */
+	var base = {
+		isArray: function (arr){
+			return (Array && Array.isArray) ? Array.isArray(arr) : Object.prototype.toString.call(arr) === '[object Array]';
+		},
+		hasKey: function(obj, key){
+			return obj !== null && hasOwnProperty.call(obj, key);
+		},
+		each: function (obj, fn){
+			var len = obj.length,i = 0;
+
+			if(base.isArray(obj)){
+				//如果是数组
+				for(; i<len; i++){
+			       	if ( false === fn.call(obj[i], obj[i], i) ){break;}
+			    }
+			}else{
+				//如果是对象
+			    for( i in obj ){
+			    	if(base.hasKey(obj, i)){
+			    		if(false === fn.call(obj[i],i+1,obj[i])){break;}
+			    	}
+			    }
+			}
+		}
+	};
 
 	/*
 		通信
 	*/
+	var communication = {
+		send: function(tag, data, cb){
+			chrome.tabs.query({active:true}, function(tab) {
+				chrome.tabs.sendMessage(tab[0].id, {
+					tag: tag,
+					data: data
+				}, function(res) {
+			        //cb && cb(res);
+			        if(cb){
+			        	cb(res);
+			        }
+			    });
+			});
+		},
+		sendListen: function(tag, cb){
+			chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+				if(request.tag == tag && cb){
+					cb(request.data, sender, sendResponse);
+				}
+			});
+		}
+
+	};
 
 	/*
 		读取页面的script内容
 	*/
+	var getPageScript = {
+		getScripts: function(){
+			return document.getElementsByTagName('script');
+		},
+		getPageScripts: function(){
+			var pageScripts = [];
+			base.each(getPageScript.getScripts(), function (i_script, v_script){
+				if(v_script.src === ""){
+					pageScripts.push(v_script.innerHTML
+						.replace(/\/\/[\S]*/g, '')
+						.replace(/[}][\s]/g, '};')
+						.replace(/var\s/g, 'var#')
+						.replace(/\s+/g,"")
+						.replace(/var#/g, 'var '));
+				}
+			});
+
+			return pageScripts;
+		},
+		getPageValue: function(valueName){
+			var value = null;
+			var allPageScriptStr = getPageScript.getPageScripts().join('');
+			var valueStart = allPageScriptStr.indexOf(valueName+'=');
+			if(valueStart == -1){
+				return value;
+			}
+
+			var valueEnd = allPageScriptStr.indexOf(';', valueStart);
+			if(valueStart + valueName.length + 1 >= valueEnd){
+				return value;
+			}
+			var valueStr = allPageScriptStr.slice(valueStart + valueName.length + 1, valueEnd);
+		
+			try{
+				value = (new Function('return '+valueStr+';'))();
+			}catch(err){}
+			return value;
+		}
+	};
 
 
 	var crxTool = {
-
-
+		send: communication.send,
+		sendListen: communication.sendListen,
+		getPageValue: getPageScript.getPageValue
 	};
-	return crxTools
 
+	return crxTool;
 
 });
